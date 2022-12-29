713b1e0d5d3d60dc3cae41c3b720e640a6e10bcd
SoumadipD <soumadipdas18@yahoo.com>
2022-12-30 04:46:15 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://verysecretapir.udgamiitg.com/",
+  baseURL: "http://verysecretapir.udgamiitg.com:3000/",
 });
 
 export default instance;
