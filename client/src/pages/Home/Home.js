b6d8625f8ad8033d608e5fceff5c973af94c7ad1
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2023-01-02 13:30:27 +0530

    Refactoring start

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+// import Events from '../../components/Home/Events'
+import Events from "../../components/Home/Events.js";
+import Navbar from "../../components/Navbar";
+import Landing from "../../components/Home/Landing";
+import PastSpeakers from "../../components/Home/Speakers_PastSpeak";
+import BuyMerch from "../../components/Home/BuyMerch";
+import GetTicket from "../../components/Home/GetTicket";
+import Footer from "../../components/Home/footer";
+import ComingSoon from "../../components/Home/ComingSoon";
+import Sponsor from "../../components/Home/sponsors";
+
+function Home() {
+	return (
+		<>
+			{/* <Navbar /> */}
+			{/* Home */}
+			<Landing />
+			{/*Events */}
+			<Events />
+			{/* Past Speakers*/}
+			<PastSpeakers />
+			{/* <ComingSoon/> */}
+			<Sponsor />
+			{/* Merch*/}
+			<BuyMerch />
+			{/* Get Ticket*/}
+			<GetTicket />
+			{/* Footer*/}
+			<Footer />
+		</>
+	);
+}
+
+export default Home;
