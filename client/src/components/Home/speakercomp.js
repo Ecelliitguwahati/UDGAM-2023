2d81f768546b1846964186db638ec848cda629a1
Your Name <you@example.com>
2023-01-05 06:04:51 +0530

    Nice

--- client/src/components/Home/speakercomp.js
+++ client/src/components/Home/speakercomp.js
@@ -4,10 +4,10 @@ import linkicon from "../../icons/Frame 34161.png"
 
 const Speakercomp =(props)=>{
     return(
-        <div className="contspeak" style={{height:`max-content`,width:`232px`}}>
+        <div className="contspeak" style={{height:`max-content`}}>
             <div style={{position:`relative`, height:`fit-content`, width:`fit-content`}}>
                 {/* <a href={props.link} target="_blank" ><img src={linkicon} style={{position:`absolute`, top:`178px`, left:`109px`}}/></a> */}
-                <img src={props.img} style={{height:`219px`, width:`213px`}}/>
+                <img src={props.img} />
             </div>
             <div style={{display:`flex`, flexDirection:`column`, justifyContent:`center`, alignContent:`center`,textAlign:`center`}} >
                 <p style={{color:`#FFFFFF`, fontSize:`32px`, fontFamily:`Action Sans`, margin:`18px 0 9px 0`, fontWeight:"700"
