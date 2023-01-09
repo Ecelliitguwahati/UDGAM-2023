70eb9fca0ac81746cc0255cac60aaaccc93afa39
raj <rajinertia9239111@gmail.com>
2023-01-10 00:20:58 +0530

    added the merch component

--- client/src/components/Home/BuyMerch.js
+++ client/src/components/Home/BuyMerch.js
@@ -4,6 +4,15 @@ import "./BuyMerch.css";
 import BuyM from "./BuyMerchButton";
 import vauc from './../../icons/vauc.svg';
 import vac from './../../icons/vac.svg';
+import hoodie from './../../icons/hoodie.png';
+import whiteTshirt from './../../icons/whiteTshirt.png';
+import blackTshirt from './../../icons/blackTshirt.png';
+import blackTshirt2 from './../../icons/blackTshirt2.png';
+import stylishlogo from './../../icons/stylishlogo.png';
+import stylishlogo2 from './../../icons/stylishlogo2.png';
+import badge from './../../icons/badge.png';
+import tshirt2 from './../../icons/tshirt2.png';
+import diary from './../../icons/diary.png';
 import { useState } from 'react';
 // import { ToastContainer, toast } from 'react-toastify';
 //   import 'react-toastify/dist/ReactToastify.css';
@@ -25,15 +34,63 @@ function BuyMerch() {
           }}><a href="/comingsoon">View All Merch <img src={img} alt="altimg" /></a>
         </button>
       </div>
-      <div class="backimg">
+      {/* <div class="backimg">
+        
 
         <div class="but_merch">
           <BuyM />
         </div>
+      </div> */}
+      <div className='merchImages'>
+          <div className='columnOne'>
+             <div className='imageOne'>
+              
+                <img src={hoodie} alt='altimg'/>
+              
+             </div>
+             <div className='imageTwo' >
+                <img src={whiteTshirt} alt='altimg'/>
+             </div>
+
+          </div>
+          <div className='columnTwo'>
+              <div className='imageThird'>
+                 <div className='ThirdOne' >
+                    <img src={blackTshirt} alt='altimg'/>
+                 </div>
+                 <div className='ThirdTwo' >
+                    <img src={blackTshirt2} alt='altimg'/>
+                 </div>
+             </div>
+             <div className='imageFour' >
+                 <div className='FourOne' >
+                   <img src={stylishlogo} alt='altimg'/>
+                   <img src={stylishlogo2} alt='altimg'/>
+                </div>
+                 <div className='FourTwo' >
+                 <img src={badge} alt='altimg'/>   
+                </div>
+             </div>
+
+          </div>
+          <div className='columnThree'>
+              <div className='imageFive'>
+              <img src={diary} alt='altimg'/>
+              </div>
+              <div className='imageSix' >
+              <img src={tshirt2} alt='altimg'/>
+              </div>
+          </div>
+          
       </div>
+      <div class="but_merch">
+          <BuyM />
+        </div>
     </div>
 
   )
 }
 
 export default BuyMerch
+
+
