25e9dcd318dc6007d007509572f3cce916d93cdf
Your Name <you@example.com>
2023-01-14 10:43:12 +0530

    AA

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-//baseURL: "https://www.udgamiitg.com/backend",
- baseURL:"http://localhost:5000/backend/"
+baseURL: "https://www.udgamiitg.com/backend",
+// baseURL:"http://localhost:5000/backend/"
 });
 
 export default instance;
